1d967f3bd7e29fe5d5e85b91350bff82e379b527
maxwell3025 <amoinus@gmail.com>
2026-04-01 20:32:41 -0400

    test: add test for typing let expressions

--- script3025_core/test/script3025_core_test.cpp
+++ script3025_core/test/script3025_core_test.cpp
@@ -91,6 +91,23 @@ TEST(type_gen_visitor, Pi_basic) {
   EXPECT_EQ(*type_gen_visitor.get_type(expression.get()), *expected_type);
 }
 
+TEST(type_gen_visitor, let_expression_easy) {
+  auto expression = script3025::text_to_expression(
+      "let (x: Nat) := 0 in x");
+  auto expected_type =
+      script3025::text_to_expression("Nat");
+  script3025::TypeGenVisitor type_gen_visitor{{}, {}};
+  type_gen_visitor.visit(*expression);
+  if (type_gen_visitor.get_type(expression.get()) == nullptr) {
+    SPDLOG_LOGGER_ERROR(get_logger(),
+                        "Type generation failed for expression:\n{}",
+                        expression->to_string());
+    FAIL() << "Type generation failed for expression:\n"
+           << expression->to_string();
+  }
+  EXPECT_EQ(*type_gen_visitor.get_type(expression.get()), *expected_type);
+}
+
 TEST(Program, simple) {
   script3025::Program program("def identity := lambda (x : Type). x");
   SPDLOG_LOGGER_INFO(get_logger(), "\n{}", program);
